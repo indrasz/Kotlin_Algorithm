f1ac594212dcfd60001dd28a9bc6a2e301ad2276
wahyuindra <wahyuindra712@gmail.com>
2022-02-26 22:36:59 +0700

    add data type

--- src/main/kotlin/data_type/Main.kt
+++ src/main/kotlin/data_type/Main.kt
@@ -1,4 +1,25 @@
 package data_type
 
-class Main {
+fun main(){
+    var variableControl1 : String = "Variable"
+    //variabel yang bisa diganti
+
+    val variableControl2 : String = "Variable"
+    //variabel yang tidak bisa diganti
+
+    /*
+       hitung jumlah penumpang
+     */
+
+    var penumpang : Int = 0
+    penumpang += 5
+    penumpang += 7
+    penumpang += 8
+    penumpang += 9
+
+    println("Jumlah penumpang adalah $penumpang \n")
+
+
+
+
 }
